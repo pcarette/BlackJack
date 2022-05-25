8a4c5b4669474201591f870b7b277ae584c69599
Pierre Carette <pierre.carette78@gmail.com>
2022-05-25 10:12:07 +0200

    now i'm starting to manipulate the DOM to integrate the game

--- js/game.js
+++ js/game.js
@@ -1,3 +1,79 @@
+//Now we declare the variables to build the game :
+const suits = ["Coeur", "Carreau", "Pique", "Trefle"];
+const values = ["As", 2, 3, 4, 5, 6, 7, 8, 9, 10, "V", "D", "R"];
+let gameDeck = [];
+let dealerVictories = 0;
+let playerVictories = 0;
+let ties = 0;
+
+// Those 2 fonctions are necessay to shuffle our 6 decks of cards :
+function fisherYatesShuffle(arr) {
+    for (let i = arr.length; i > 0; i--) {
+      const j = Math.floor(Math.random() * i)
+      const temp = arr[j]
+      arr[j] = arr[i - 1]
+      arr[i - 1] = temp
+    }
+    return arr;
+  }
+
+function getRandomSelection(n, array) {
+    const cloned = Array.from(array)
+    const shuffled = fisherYatesShuffle(array)
+    const selected = shuffled.slice(0, n)
+    return selected
+  }
+
+//We build a Card class to build our cards
+
+class Card {
+    constructor(suit, value) {
+        this.suit = suit;
+        this.value = value;
+        this.power;
+        this.image;
+        if (typeof value === "number") {
+            this.power = value;
+        } else if (value === "V" || value === "D" || value === "R") {
+            this.power = 10;
+        } else {
+            this.power = 1;
+        }
+    }
+    displayCard() {
+        
+    }
+}
+
+//Then we make our deck
+
+function makeADeck() {
+    let deck = [];
+    for (let i = 0; i < suits.length; ++i) {
+        for (let j = 0; j < values.length; ++j) {
+            const card = new Card(suits[i], values[j]);
+            deck.push(card);
+        }
+    } return deck;
+}
+
+//for this game, we will reinitialize our decks each round, but to avoid predictibility
+//we will shuffle 6 decks of cards :
+
+function make6Decks() {
+    for (let i = 0; i < 6; ++i) {
+        gameDeck.push(makeADeck());
+    }
+    gameDeck = gameDeck.flat()
+}
+
+make6Decks();
+
+function shuffleDeck() {
+    return getRandomSelection(gameDeck.length, gameDeck);
+}
+
+
 class Player {
     constructor() {
         this.score = 0;
@@ -15,12 +91,24 @@ class Dealer {
         this.score = 0;
         this.deck = [];
     } 
-    pickCardDealer(card) {
+    pickCard(card) {
         this.deck.push(card);
         console.log(this.deck);
-        this.dealer.score += this.deck[this.deck.length-1].power;
+        this.score += this.deck[this.deck.length-1].power;
         console.log(`The dealer is now at ${this.score}.`);
     }
+
+    pickWithoutDisplay(card) {
+        this.deck.push(card);
+    }
+    
+    calculateScore() {
+        this.score = 0;
+        for (let i = 0; i < this.deck.length; ++i) {
+            this.score += this.deck[i].power;
+        }
+    }
+    
 }
 
 class Game {
@@ -39,24 +127,12 @@ class Game {
     }
 
    
-    pickWithoutDisplayDealer() {
-        this.dealer.deck.push(this.tableDeck.pop());
-    }
-    
-    revealDealer() {
-        this.dealer.score = 0;
-        for (let i = 0; i < this.dealer.deck.length; ++i) {
-            this.dealer.score += this.dealer.deck[i].power
-        }
-    }
-    
-    playTheGame() {
-        this.pickCardPlayer(this.removeTopCard());
-        this.pickCardDealer(this.removeTopCard());
-        this.pickCardPlayer(this.removeTopCard());
-        this.pickWithoutDisplayDealer();
-        let callPlayer = prompt("")
-        if (callPlayer == "Y" ){}
+    startHand() {
+        this.player.pickCard(this.removeTopCard());
+        this.dealer.pickCard(this.removeTopCard());
+        this.player.pickCard(this.removeTopCard());
+        this.dealer.pickWithoutDisplay(this.removeTopCard());
+        
     }
     
     }
--- js/index.js
+++ js/index.js
@@ -4,86 +4,14 @@
 const startGame = document.getElementById("start-game");
 const menuDisplayer = document.querySelector(".menu-displayer");
 const damier = document.querySelector(".damier");
+let userMessage = document.querySelector(".user-message").lastChild.textContent
 startGame.addEventListener("click", () => {
     menuDisplayer.classList.add("hidden");
     damier.classList.remove("hidden");
+    newGame.startHand();
 })
-//Now we declare the variables to build the game :
-const suits = ["Coeur", "Carreau", "Pique", "Trefle"];
-const values = ["As", 2, 3, 4, 5, 6, 7, 8, 9, 10, "V", "D", "R"];
-let gameDeck = [];
-let dealerVictories = 0;
-let playerVictories = 0;
-let ties = 0;
 
-// Those 2 fonctions are necessay to shuffle our 6 decks of cards :
-function fisherYatesShuffle(arr) {
-    for (let i = arr.length; i > 0; i--) {
-      const j = Math.floor(Math.random() * i)
-      const temp = arr[j]
-      arr[j] = arr[i - 1]
-      arr[i - 1] = temp
-    }
-    return arr;
-  }
-
-function getRandomSelection(n, array) {
-    const cloned = Array.from(array)
-    const shuffled = fisherYatesShuffle(array)
-    const selected = shuffled.slice(0, n)
-    return selected
-  }
-
-//We build a Card class to build our cards
-
-class Card {
-    constructor(suit, value) {
-        this.suit = suit;
-        this.value = value;
-        this.power;
-        this.image
-        if (typeof value === "number") {
-            this.power = value;
-        } else if (value === "V" || value === "D" || value === "R") {
-            this.power = 10;
-        } else {
-            this.power = 1;
-        }
-    }
-}
-
-//Then we make our deck
-
-function makeADeck() {
-    let deck = [];
-    for (let i = 0; i < suits.length; ++i) {
-        for (let j = 0; j < values.length; ++j) {
-            const card = new Card(suits[i], values[j]);
-            deck.push(card);
-        }
-    } return deck;
-}
-
-//for this game, we will reinitialize our decks each round, but to avoid predictibility
-//we will shuffle 6 decks of cards :
-
-function make6Decks() {
-    for (let i = 0; i < 6; ++i) {
-        gameDeck.push(makeADeck());
-    }
-    gameDeck = gameDeck.flat()
-}
-
-make6Decks();
-
-function shuffleDeck() {
-    return getRandomSelection(gameDeck.length, gameDeck)
-}
-
-// console.log(pickCards())
-// console.log(gameDeck.length);
-console.log(shuffleDeck())
-// console.log(gameDeck.length);
+document.querySelector(".user-message").lastChild.textContent = "What do you want to do"
 
 // The game interface :
 const newGame = new Game();
